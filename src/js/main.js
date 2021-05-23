44e62583948f7f08d375e9ad5a1d2a0edc8373e0
Dmitry Murzha <dmitry.murzha@gmail.com>
2021-05-24 02:46:44 +0300

    Menu is ready.

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function ($) {
 
     // Sliders
     $('.homepage-main__slider').slick({
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 7000,
         dots: false,
         arrows: false,
@@ -24,7 +24,7 @@ jQuery(document).ready(function ($) {
         dots: false,
         arrows: true,
         infinite: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3200,
         slidesToShow: 4,
         slidesToScroll: 2,
@@ -39,7 +39,7 @@ jQuery(document).ready(function ($) {
                 breakpoint: 576,
                 settings: {
                     slidesToShow: 2,
-                    rows:3,
+                    rows: 3,
                 }
             },
         ]
@@ -50,7 +50,7 @@ jQuery(document).ready(function ($) {
         dots: false,
         arrows: true,
         infinite: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4400,
         slidesToShow: 3,
         slidesToScroll: 2,
@@ -81,7 +81,7 @@ jQuery(document).ready(function ($) {
     })
 
     //Header menu
-    $(".dropdown-content .dropdown-item.dropdown").hover(
+    $(".dropdown-content .dropdown-item.dropdown").bind('hover',
         function () {
             $(".dropdown-content .dropdown-item.dropdown").removeClass("active")
             $(this).addClass("active")
@@ -93,13 +93,38 @@ jQuery(document).ready(function ($) {
 
     //Mobile menu
     let hamburger = $('.menu__hamburger');
+    let menuItems = $('.menu__items');
+    let navBar = $('.nav');
 
     hamburger.on('click', function () {
-        if (hamburger.hasClass('menu__hamburger-active')) {
+        if (hamburger.hasClass('menu__hamburger-active') && menuItems.hasClass('menu__items__active')) {
+            navBar.removeClass('nav__menu-active');
             hamburger.removeClass('menu__hamburger-active');
-
+            menuItems.removeClass('menu__items__active');
+            $('body').removeClass('disable-scroll');
+            menuItems.parent().find('.dropdown-link').removeClass('active')
+            menuItems.parent().find('.menu__link-mobile-arrow').removeClass('active')
+            menuItems.parent().find('.dropdown-content').slideUp(300).delay(200);
         } else {
+            navBar.addClass('nav__menu-active');
             hamburger.addClass('menu__hamburger-active');
+            menuItems.addClass('menu__items__active');
+            $('body').addClass('disable-scroll');
+        }
+    });
+
+    $('.menu__link.dropdown .dropdown-link').on('click', function (event) {
+        event.preventDefault();
+        if ($(this).next('.dropdown-content').css('display') === 'none') {
+            $(this).next('.dropdown-content').slideDown(300).delay(200);
+            $(this).addClass('active');
+            $(this).find('.menu__link-mobile-arrow').addClass('active')
+        } else {
+            $(this).next('.dropdown-content').slideUp(300).delay(200);
+            $(this).removeClass('active');
+            $(this).find('.menu__link-mobile-arrow').removeClass('active')
+            $(this).parent().find('.dropdown-link').removeClass('active');
+            $(this).parent().find('.dropdown-content').slideUp(300).delay(200);
         }
     });
 })
